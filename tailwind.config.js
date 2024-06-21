32b7b180370b9d620215348c85dc4d9134a13c3d
Marco Kucznierz <m.kucznierz@slenderiser.de>
2024-06-21 20:15:48 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        flixBlack: '#181832',
+        flixGreen: '#64e1af',
+        flixGrey: '#f3f2f2',
+        flixYellow: '#f0c84b'
+      },
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+}
