9eef6696a0395af37680afc8ccfe8a6415decbc3
Marco Kucznierz <m.kucznierz@slenderiser.de>
2024-06-23 09:20:22 +0200

    added card component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import FormCard from './components/FormCard.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <form-card></form-card>
 </template>
 
 <style scoped>
---
+++ src/components/FormCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="rounded bg-flixGrey py-10 px-12 text-left">
+    
+    <div class="px-6 py-4">
+      <div class="text-3xl font-bold underline text-flixBlack">IP Geo Lookup</div>
+      <p class="text-flixBlack ">
+        send a request to <a className="underline text-flixGreen" rel="noreferrer" href='http://ip-api.com/' target='_blank'>ip-api.com</a>
+      </p>
+    </div>
+    <!---- Platz für Input komponenten --->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+};
+</script>
+
+<style scoped>
+/* Zusätzliche Styles können hier hinzugefügt werden */
+</style>
